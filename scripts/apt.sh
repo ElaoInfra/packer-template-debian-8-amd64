574b859cadefd0c2bf6cd19451c6bbd8743bb195
nervo <nervo@nervo.net>
2015-10-18 13:58:59 +0200

    Force httpredir and contrib/non-free by script

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+cat <<EOF > /etc/apt/sources.list
+deb http://httpredir.debian.org/debian jessie main non-free contrib
+deb-src http://httpredir.debian.org/debian jessie main non-free contrib
+
+deb http://security.debian.org/ jessie/updates main contrib non-free
+deb-src http://security.debian.org/ jessie/updates main contrib non-free
+
+# jessie-updates, previously known as 'volatile'
+deb http://httpredir.debian.org/debian jessie-updates main contrib non-free
+deb-src http://httpredir.debian.org/debian jessie-updates main contrib non-free
+EOF
+
 apt-get update
